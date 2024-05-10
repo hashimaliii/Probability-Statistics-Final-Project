940a071c102111c3666750240ea88e2fe3f7466e
Muhammad Hashim Ali <hashimtrox@gmail.com>
2024-05-10 17:10:24 +0500

    Update func.py

--- func.py
+++ func.py
@@ -1,217 +1,283 @@
+# Import necessary libraries for data manipulation, statistical analysis, and visualization
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn
-import altair as alt
 import scipy.stats as stats
+
+# Import machine learning libraries from sklearn for regression analysis
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+
+# Import train_test_split for splitting the dataset into training and testing sets
 from sklearn.model_selection import train_test_split
-from scipy.stats import poisson, gaussian_kde
+
+# Import poisson from scipy.stats for Poisson distribution
+from scipy.stats import poisson
+
+# Import LabelEncoder for encoding categorical variables
 from sklearn.preprocessing import LabelEncoder
+
+# Import statsmodels for building and analyzing statistical models
+import statsmodels.api as sm
+
+# Import stats from scipy for statistical functions
+from scipy import stats
+
+# Ignore warnings
 import warnings
 warnings.filterwarnings('ignore')
 
-def load_data():
-    return pd.DataFrame(pd.read_csv(r'dataset/Employee_Salaries.csv'))
+# Function to load data from a CSV file located at a specific URL
+def load_data_from_github():
+    url = 'https://raw.githubusercontent.com/hashimaliii/Probability-Statistics-Final-Project/main/dataset/Employee_Salaries.csv'
+    # Return the loaded data as a pandas DataFrame
+    return(pd.read_csv(url))
 
-def plot_line_chart(df):
-    plt.figure(figsize=(10,10))
-    x = df['Department_Name']
-    y = df['Base_Salary']
+# Function to load data from a CSV file into a DataFrame
+def load_data_from_csv():
+    # Use pandas read_csv function to read the CSV file
+    # and return the resulting DataFrame
+    return pd.read_csv('dataset/Employee_Salaries.csv')
 
-    plt.plot(x,y)
-    plt.xlabel('Department_Name')
-    plt.ylabel('Base_Salary')
-    plt.title('Employee Salaries', loc='center')
-    plt.grid(True)
-    plt.xticks(rotation=90, ha='right')
-    plt.show()
+# Function to add a new record to the DataFrame
+def add_record(df):
+    # Prompt the user for input for each field in the record
+    Department = input("Enter Department: ")
+    Department_Name = input("Enter Department Name: ")
+    Division = input("Enter Division: ")
+    Gender = input("Enter Gender (M/F): ")
+    Base_Salary = float(input("Enter Base Salary: "))  # Convert input to float
+    Overtime_Pay = float(input("Enter Overtime Pay: "))  # Convert input to float
+    Longevity_Pay = float(input("Enter Longevity Pay: "))  # Convert input to float
+    Grade = input("Enter Grade: ")
 
+    # Create a dictionary from the user's input
+    new_record = {'Department': Department, 'Department_Name': Department_Name, 'Division': Division, 'Gender': Gender, 'Base_Salary': Base_Salary, 'Overtime_Pay': Overtime_Pay, 'Longevity_Pay': Longevity_Pay, 'Grade': Grade}
+
+    # Add the new record to the DataFrame at the end
+    df.loc[len(df)] = new_record
+
+    # Return the updated DataFrame
+    return df
+
+palette = seaborn.color_palette("Reds", 4)
+
+# Function to plot department-wise salary distribution
 def plot_depwise_salary_distribution(df):
-    # Visualization : Department-wise salary distribution
+    # Create a new figure with specified size
     plt.figure(figsize=(10, 14))
-    df.groupby('Department_Name')['Base_Salary'].mean().sort_values().plot(kind='barh', color=['#6a51a3', '#807dba', '#3f007d', '#54278f'])
+    # Group the DataFrame by 'Department_Name', calculate the mean of 'Base_Salary' for each group,
+    # sort the values, and plot a horizontal bar chart
+    df.groupby('Department_Name')['Base_Salary'].mean().sort_values().plot(kind='barh', color=palette)
+    # Set the title of the plot
     plt.title('Average Salary by Department')
+    # Set the label for the x-axis
     plt.xlabel('Average Salary')
+    # Set the label for the y-axis
     plt.ylabel('Department')
+    # Display the plot
     plt.show()
 
+# Function to plot a pie chart of gender distribution
 def plot_pie_chart(df):
-    # Visualization : Pie chart of gender distribution
+    # Create a new figure with specified size
     plt.figure(figsize=(8, 6))
-    df['Gender'].value_counts().plot(kind='pie', autopct='%1.2f%%', colors=['#6a51a3', '#807dba', '#3f007d', '#54278f'])
+    # Count the occurrences of each unique value in the 'Gender' column,
+    # and plot a pie chart with percentage labels
+    df['Gender'].value_counts().plot(kind='pie', autopct='%1.2f%%', colors=palette)
+    # Set the title of the plot
     plt.title('Gender Distribution')
+    # Remove the label for the y-axis
     plt.ylabel('')
+    # Display the plot
     plt.show()
 
+# Function to plot a bar chart of gender distribution
 def plot_bar_chart(df):
+    # Create a new figure with specified size
     plt.figure(figsize=(8, 6))
-    df['Gender'].value_counts().plot(kind='bar', color=['#6a51a3', '#807dba', '#3f007d', '#54278f'])
+    # Count the occurrences of each unique value in the 'Gender' column,
+    # and plot a bar chart
+    df['Gender'].value_counts().plot(kind='bar', color=palette)
+    # Set the title of the plot
     plt.title('Gender Distribution of Employees')
+    # Set the label for the x-axis
     plt.xlabel('Gender')
+    # Set the label for the y-axis
     plt.ylabel('Count')
+    # Display the plot
     plt.show()
 
+# Function to plot pairwise scatter plot of numerical variables
 def plot_scatter_pairwise(df):
-    # Visualization 8: Pairwise scatter plot of numerical variables
-    seaborn.pairplot(df[['Base_Salary', 'Overtime_Pay', 'Longevity_Pay']], diag_kind='kde', plot_kws={'color': '#807dba'})
+    # Use seaborn's pairplot function to create a grid of scatter plots
+    seaborn.pairplot(df[['Base_Salary', 'Overtime_Pay', 'Longevity_Pay']], diag_kind='kde', plot_kws={'color': palette[0]})
     plt.show()
 
+# Function to display descriptive statistics of employee salaries
 def plot_descriptive_statistics(df):
+    # Get only the numeric columns from the DataFrame
     numeric_df = df._get_numeric_data()
 
-    print("\nDescriptive Statistics of Employee Salaries:")
+    # Calculate and print descriptive statistics
     descriptiveStateDF = numeric_df.describe()
 
-    # Median
+    # Append median to the descriptive statistics DataFrame
     descriptiveStateDF = pd.concat([descriptiveStateDF,median(numeric_df)],axis = 0)
     descriptiveStateDF.rename(index={0:'median'}, inplace=True)
     
-    # Variance
+    # Append variance to the descriptive statistics DataFrame
     descriptiveStateDF = pd.concat([descriptiveStateDF,variance(descriptiveStateDF)],axis = 0)
     descriptiveStateDF.rename(index={0:'variance'}, inplace=True)
 
     print(descriptiveStateDF)
 
+    # Print mode statistics of employee salaries
     print("\nMode Statistics of Employee Salaries:")
     print(descriptiveStateDF.mode())
 
+# Function to calculate median of salary components
 def median(df):
-    baseSalary = df['Base_Salary'].median()
-    overtimePay = df['Overtime_Pay'].median()
-    longevityPay = df['Longevity_Pay'].median()
-    medianDF = pd.DataFrame([{'Base_Salary':baseSalary, 'Overtime_Pay':overtimePay, 'Longevity_Pay':longevityPay}])
+    # Calculate median for each salary component and return as DataFrame
+    medianDF = pd.DataFrame([{'Base_Salary':df['Base_Salary'].median(), 'Overtime_Pay':df['Overtime_Pay'].median(), 'Longevity_Pay':df['Longevity_Pay'].median()}])
     return medianDF
 
+# Function to calculate variance of salary components
 def variance(df):
-    baseSalary = float(df['Base_Salary']['std'])**2
-    overtimePay = float(df['Overtime_Pay']['std'])**2
-    longevityPay = float(df['Longevity_Pay']['std'])**2
-    varianceDF = pd.DataFrame([{'Base_Salary':baseSalary, 'Overtime_Pay':overtimePay, 'Longevity_Pay':longevityPay}])
+    # Calculate variance for each salary component and return as DataFrame
+    varianceDF = pd.DataFrame([{'Base_Salary':float(df['Base_Salary']['std'])**2, 'Overtime_Pay':float(df['Overtime_Pay']['std'])**2, 'Longevity_Pay':float(df['Longevity_Pay']['std'])**2}])
     return varianceDF
 
+# Function to plot a box plot of 'Base_Salary' by 'Gender'
 def boxPlot(df):
-    # Visualization : Box plot of base salary by gender
-    df.boxplot(column='Base_Salary', by='Gender', grid=False, color=dict(boxes='#6a51a3', whiskers='#807dba', medians='#3f007d', caps='#54278f'))
+    df.boxplot(column='Base_Salary', by='Gender', grid=False, color=dict(boxes="#990000", whiskers="#ff0000", medians="#ff3333", caps="#ff6666"))
     plt.title('Base Salary by Gender')
     plt.xlabel('Gender')
     plt.ylabel('Base Salary')
     plt.show()
 
-def plot_heat_map_correlation(df):
+# Function to plot a heatmap of the correlation matrix
+def plot_heat_map(df):
     plt.figure(figsize=(10, 10))
     numeric_df = df._get_numeric_data()
     seaborn.heatmap(numeric_df.corr(), annot=True)
     plt.title('Pairwise correlation of columns', fontsize=16)
-    plt.title('Pairwise correlation of columns', loc='center')
     plt.show()
 
+# Function to plot histograms and skewness of salary components
 def plot_Skew(df):
     fig, axs = plt.subplots(1, 3, figsize=(12, 10))
     salaries = ['Base_Salary', 'Overtime_Pay', 'Longevity_Pay']
-    data = [df[salary] for salary in salaries]
-    for i, ax in enumerate(axs.flat):
-        seaborn.histplot(data[i], bins=20, color='skyblue', ax=ax, edgecolor='black', kde=True)
-        skewness = data[i].skew()
-        ax.set_title(f'{salaries[i]} (Skew: {skewness:.2f})', fontstyle='italic')
-
-        if skewness > 0:
-            ax.text(0.05, 0.9, 'Right Skew', transform=ax.transAxes, fontsize=10, bbox=dict(facecolor='orange', alpha=0.5))
-        elif skewness < 0:
-            ax.text(0.05, 0.9, 'Left Skew', transform=ax.transAxes, fontsize=10, bbox=dict(facecolor='orange', alpha=0.5))
-        else:
-            ax.text(0.05, 0.9, 'No Skew', transform=ax.transAxes, fontsize=10, bbox=dict(facecolor='orange', alpha=0.5))
-
+    skew_label_func = lambda skew: 'Right Skew' if skew > 0 else ('Left Skew' if skew < 0 else 'No Skew')
+    for salary, ax in zip(salaries, axs):
+        seaborn.histplot(df[salary], bins=20, color='green', ax=ax, edgecolor='black', kde=True)
+        skewness = df[salary].skew()
+        ax.set_title(f'{salary} (Skew: {skewness:.2f})', fontstyle='italic')
+        ax.text(0.05, 0.9, skew_label_func(skewness), transform=ax.transAxes, fontsize=10, bbox=dict(facecolor='red', alpha=0.5))
         ax.set_xlabel('Value')
         ax.set_ylabel('Frequency')
-
     plt.show()
 
-def labelencoder(df):
+# Function to display the correlation matrix
+def display_correlation(df):
+    df = df[['Overtime_Pay', 'Longevity_Pay', 'Base_Salary']]
+    df = df._get_numeric_data()
+    correlation_matrix = df.corr()
+    print("\nCorrelation Matrix:")
+    print(correlation_matrix)
+
+# Function to display the covariance matrix
+def display_covariance(df):
+    df = df[['Overtime_Pay', 'Longevity_Pay', 'Base_Salary']]
+    df = df._get_numeric_data()
+    covariance_matrix = np.cov(df, rowvar=False)
+    cov_df = pd.DataFrame(covariance_matrix, columns=df.columns, index=df.columns)
+    print("\nCovariance Matrix:")
+    print(cov_df)
+
+# Function to fit a regression model and perform hypothesis testing
+def display_regression_hypothesisTesting(df):
+    X = df[['Overtime_Pay', 'Longevity_Pay']]  # Independent variables
+    y = df['Base_Salary']  # Dependent variable
+    X = sm.add_constant(X)  # Adds a constant term to the predictor
+    model = sm.OLS(y, X).fit()
+    t_test = model.t_test('Overtime_Pay = 0')  # Test if the coefficient of 'Overtime_Pay' is zero
+    print(model.summary())
+    print(t_test)
+
+def predict_regression_model(df, confidence):
+    # Encode categorical variables
     for c in df.columns:
-        if df[c].dtype=='object': 
+        if df[c].dtype == 'object': 
             df[c] = df[c].fillna('N')
             lbl = LabelEncoder()
             lbl.fit(list(df[c].values))
             df[c] = lbl.transform(df[c].values)
-    return df
 
-def preprocess_data(df):
-    labelencoder(df)
-    targetData = ['Grade']
-    X = df.drop(targetData, axis = 1)
+    # Define target variable
+    target_data = ['Grade']
+    X = df.drop(target_data, axis=1)
     y = df['Grade']
+
+    # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    return X_train, X_test, y_train, y_test
 
-def train_models(X_train, y_train):
-    linear_reg = LinearRegression()
-    rf_reg = RandomForestRegressor()
-    gb_reg = GradientBoostingRegressor()
-    linear_reg.fit(X_train, y_train)
-    rf_reg.fit(X_train, y_train)
-    gb_reg.fit(X_train, y_train)
-    return {'Linear Regression': linear_reg, 'Random Forest': rf_reg, 'Gradient Boosting': gb_reg}
+    # Define regression model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    train_score = model.score(X_train, y_train)
+    test_score = model.score(X_test, y_test)
+    y_pred = model.predict(X_test)
+    prediction_score = model.score(X_test, y_pred)
+    original_score = model.score(X_test, y_test)
 
-def evaluate_models(models, X_train, X_test, y_train, y_test,confidance):
-    results = {}
-    for name, model in models.items():
-        train_score = model.score(X_train, y_train)
-        test_score = model.score(X_test, y_test)
-        y_pred = model.predict(X_test)
-        prediction_score = model.score(X_test, y_pred)
-        original_score = model.score(X_test, y_test)
-        n = len(y_test)
-        std_err = np.std(y_test - y_pred)
-        margin_err = std_err * stats.t.ppf((1 + (confidance/100)) / 2, n - 1)
-        lower_bound = y_pred - margin_err
-        upper_bound = y_pred + margin_err
+    # Calculate confidence intervals
+    n = len(y_test)
+    std_err = np.std(y_test - y_pred)
+    margin_err = std_err * stats.t.ppf((1 + (confidence / 100)) / 2, n - 1)
+    lower_bound = y_pred - margin_err
+    upper_bound = y_pred + margin_err
 
-        results[name] = {'Train Score': train_score, 'Test Score': test_score, 
-                         'Prediction Score': prediction_score, 'Original Score': original_score,
-                         'Lower Bound': lower_bound, 'Upper Bound': upper_bound}
-    return results
+    # Print results
+    print("Model: Linear Regression")
+    print("Train Score: {:.2f}".format(train_score))
+    print("Test Score: {:.2f}".format(test_score))
+    print("Prediction Score: {:.2f}".format(prediction_score))
+    print("Original Score: {:.2f}".format(original_score))
+    print()
 
-def train_predict_regression_model(df,confidance):
-    X_train, X_test, y_train, y_test = preprocess_data(df)
-    models = train_models(X_train, y_train)
-    evaluation_results = evaluate_models(models, X_train, X_test, y_train, y_test,confidance)
-    return models, evaluation_results
-
-def plot_confidence_interval(lower_bound, upper_bound, confidence):
+    # Plot histograms of confidence intervals
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-    counts, bins,_  = ax1.hist(lower_bound, bins=20, density=True, alpha=0.7, color='b')
+    counts, bins,_  = ax1.hist(lower_bound, bins=20, density=True, alpha=0.7, color='y')
     ax1.set_title('Lower Bound Confidence Interval')
     ax1.set_xlabel('Value')
     ax1.set_ylabel('Density')
 
+    # Plot Poisson distribution
     mu_lower = np.mean(lower_bound)
     poisson_dist_lower = poisson(mu_lower)
     x_lower = np.arange(0, max(bins), 1)
-    ax1.plot(x_lower, poisson_dist_lower.pmf(x_lower), color='r', label='Poisson Distribution')
+    ax1.plot(x_lower, poisson_dist_lower.pmf(x_lower), color='b', label='Poisson Distribution')
     ax1.legend()
 
-    counts, bins,_  = ax2.hist(upper_bound, bins=20, density=True, alpha=0.7, color='g')
+    counts, bins,_  = ax2.hist(upper_bound, bins=20, density=True, alpha=0.7, color='r')
     ax2.set_title('Upper Bound Confidence Interval')
     ax2.set_xlabel('Value')
     ax2.set_ylabel('Density')
 
+    # Plot Poisson distribution
     mu_upper = np.mean(upper_bound)
     poisson_dist_upper = poisson(mu_upper)
     x_upper = np.arange(0, max(bins), 1)
-    ax2.plot(x_upper, poisson_dist_upper.pmf(x_upper), color='r', label='Poisson Distribution')
+    ax2.plot(x_upper, poisson_dist_upper.pmf(x_upper), color='g', label='Poisson Distribution')
     ax2.legend()
     plt.tight_layout()
     plt.show()
 
-def prediction_results(df,confidance):
-    models, evaluation_results = train_predict_regression_model(df,confidance)
-    for name, metrics in evaluation_results.items():
-        print(f"Model: {name}")
-        print(f"Train Score: {metrics['Train Score']:.2f}")
-        print(f"Test Score: {metrics['Test Score']:.2f}")
-        print(f"Prediction Score: {metrics['Prediction Score']:.2f}")
-        print(f"Original Score: {metrics['Original Score']:.2f}")
-        print()
-    plot_confidence_interval(metrics['Lower Bound'],metrics['Upper Bound'],confidance)
+# Function to clear the console screen
+def clear_screen():
+    clear = lambda: os.system('cls')
+    clear()
